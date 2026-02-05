d2fa2f12d0d87cc47535753824d6e01b296c5a2e
GamePartsOnline <cfredeu@gmail.com>
2026-02-05 21:37:58 +0100

    exo 06 - update

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -11,8 +11,14 @@ def translate_word(word)
     return word + "ay"
   end
   
-  # Sinon, trouve la première voyelle
+  # Trouve la première voyelle
   index = word.chars.find_index { |char| voyelles.include?(char) }
+  
+  # CAS SPÉCIAL : si c'est un "u" après un "q", cherche la voyelle suivante
+  if index > 0 && word[index] == 'u' && word[index - 1] == 'q'
+    # Cherche la voyelle APRÈS ce "qu"
+    index = word.chars.find_index.with_index { |char, i| i > index && voyelles.include?(char) }
+  end
   
   # Déplace les consonnes du début vers la fin + "ay"
   word[index..-1] + word[0...index] + "ay"
